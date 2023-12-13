fdd8c3dfeb2c35f55fb03ef3d5241789882dba46
itsphy <153685923+itsphy@users.noreply.github.com>
2023-12-13 23:14:28 +0100

    scoreboard_ver1

---
+++ index.js
@@ -0,0 +1,41 @@
+let scoreElHome = document.getElementById('score_home')
+let scoreElGuest = document.getElementById('score_guest')
+//console.log(scoreEl)
+let scoreHome = 0
+let scoreGuest = 0
+
+//home
+function add1h() {
+    scoreHome += 1
+    scoreElHome.textContent = scoreHome
+}
+function add2h() {
+    scoreHome += 2
+    scoreElHome.textContent = scoreHome
+}
+function add3h() {
+    scoreHome += 3
+    scoreElHome.textContent = scoreHome
+}
+
+//Guest
+function add1g() {
+    scoreGuest += 1
+    scoreElGuest.textContent = scoreGuest
+}
+function add2g() {
+    scoreGuest += 2
+    scoreElGuest.textContent = scoreGuest
+}
+function add3g() {
+    scoreGuest += 3
+    scoreElGuest.textContent = scoreGuest
+}
+
+//reset
+function reset() {
+    scoreGuest = 0
+    scoreHome = 0
+    scoreElGuest.textContent = scoreGuest
+    scoreElHome.textContent = scoreHome
+}
